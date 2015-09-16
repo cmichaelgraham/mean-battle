b04e06f7174f68d2210a3e7c01b1383626b987c2
Mike Graham <mgraham@technicalmedia.com>
2015-09-16 05:13:03 -0600

    added static content, bootstrap, and styling

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
