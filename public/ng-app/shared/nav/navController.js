a042406969805ebaa87491bd3c0b0ed10f17fb28
Mike Graham <mgraham@technicalmedia.com>
2015-09-20 05:08:45 -0600

    highlight active nav item working

--- public/ng-app/shared/nav/navController.js
+++ public/ng-app/shared/nav/navController.js
@@ -2,10 +2,16 @@
 
 	module
 	.controller('NavCtrl', 
-		function(NavService) {
+		function(NavService, $location) {
+
 			this.message = 'nav-master';
 
 			this.navSvc = NavService;
+
+			this.isCurrentPath = function(path) {
+				return $location.path() == path;
+			}
+			
 		});
 
 }(angular.module('app')));
